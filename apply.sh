eee89aa4037154bbda476edc515ab7a29cf9a2bd
penglezos <panagiotisegl@gmail.com>
2022-12-21 00:39:43 +0200

    Add apply patches script
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

---
+++ apply.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (C) 2022 penglezos <panagiotisegl@gmail.com>
+#
+# Apply Android patches 
+#
+
+echo $1
+rootdirectory="$PWD"
+
+dirs=""
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo "Applying $dir patches..."
+	git apply $rootdirectory/patches/$dir/*.patch
+done
+
+echo "Patches applied. Changing to root directory."
+cd $rootdirectory
