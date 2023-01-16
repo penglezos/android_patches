e57ab057733d6f67cb12f2b235c6b83a8bc0cd58
penglezos <panagiotisegl@gmail.com>
2023-01-16 20:51:06 +0200

    apply: Add build/make directory
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- apply.sh
+++ apply.sh
@@ -8,7 +8,7 @@
 echo $1
 rootdirectory="$PWD"
 
-dirs="frameworks/base packages/apps/Settings packages/apps/Updater packages/modules/Wifi lineage-sdk system/core"
+dirs="build/make frameworks/base packages/apps/Settings packages/apps/Updater packages/modules/Wifi lineage-sdk system/core"
 
 for dir in $dirs ; do
 	cd $rootdirectory
